cb9f650a673b95e5a11eb7107f780a8ccf1aaa40
Johnathan Runyon <83104465+johnathanbrunyon@users.noreply.github.com>
2021-08-16 16:40:39 -0400

    Add Inventory class

--- app.py
+++ app.py
@@ -4,7 +4,19 @@
 # Create an Inventory class
 
 class Product:
-    def __init__(self, _id, _name, _price):
+    def __init__(self, _id, _name, _price, _inven):
         self.id = _id
         self.name = _name
         self.price = _price
+        _inven.AddProduct(self)
+
+class Inventory:
+    def __init__(self):
+        self.invenList = []
+    def AddProduct(self, p):
+        self.invenList.append(p)
+    def PrintInventory(self):
+        print("ID\t\t\t\tName\t\t\t\tPrice")
+        print("---------------------------------------------------------------------------------")
+        for p in self.invenList:
+            print(str(p.id) + "\t\t\t\t" + p.name + "\t\t\t$" + str(p.price))
