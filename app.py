e8d2086a784bfe5b9f352c0b0f762d31eb1eb96e
Johnathan Runyon <83104465+johnathanbrunyon@users.noreply.github.com>
2021-08-16 16:27:12 -0400

    Created product class

---
+++ app.py
@@ -0,0 +1,10 @@
+# TODO:
+# Create a list to store products
+# Create a product 'class'
+# Create an Inventory class
+
+class Product:
+    def __init__(self, _id, _name, _price):
+        self.id = _id
+        self.name = _name
+        self.price = _price
